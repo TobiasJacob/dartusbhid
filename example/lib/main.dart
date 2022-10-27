c3bd99f001a15d27107cbad0b5ecaeaa2331a561
Tobias Jacob <tjacob@ihmc.us>
2022-10-27 14:10:03 +0200

    Demo ui

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,9 @@
+import 'package:dartusbhid/usb_device.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:dartusbhid/dartusbhid.dart' as dartusbhid;
 import 'package:dartusbhid/enumerate.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -22,28 +22,27 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-
-    monitorDevices();
   }
 
-  void monitorDevices() async {
+  Future<List<USBDeviceInfo>> getDevices() async {
     var devices = await enumerateDevices(0, 0);
-    for (var device in devices) {
-      print(device.manufacturerString);
-      print(device.productString);
-      print(device.productId);
-      print(device.vendorId);
-    }
-    var openDevice = await devices[0].open();
-    print(devices[0].manufacturerString);
-    while (true) {
-      var report = await openDevice.readReport();
-      setState(() {
-        text = report.toString();
-      });
-    }
-    await openDevice.close();
-    print(devices[0]);
+    return devices;
+    // for (var device in devices) {
+    //   print(device.manufacturerString);
+    //   print(device.productString);
+    //   print(device.productId);
+    //   print(device.vendorId);
+    // }
+    // var openDevice = await devices[0].open();
+    // print(devices[0].manufacturerString);
+    // while (true) {
+    //   var report = await openDevice.readReport();
+    //   setState(() {
+    //     text = report.toString();
+    //   });
+    // }
+    // await openDevice.close();
+    // print(devices[0]);
   }
 
   @override
@@ -53,17 +52,36 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Native Packages'),
         ),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                Text(
-                  text,
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
+        body: Container(
+          padding: const EdgeInsets.all(10),
+          child: FutureBuilder<List<USBDeviceInfo>>(
+            future: getDevices(),
+            builder: (BuildContext context,
+                AsyncSnapshot<List<USBDeviceInfo>> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: Text('Refreshing usb devices'));
+              } else {
+                if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else {
+                  return ListView.builder(
+                      padding: const EdgeInsets.all(8),
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        var device = snapshot.data![index];
+                        return Container(
+                          height: 50,
+                          margin: const EdgeInsets.all(2),
+                          child: Center(
+                              child: Text(
+                            '${device.productString} (${device.manufacturerString})',
+                            style: const TextStyle(fontSize: 18),
+                          )),
+                        );
+                      });
+                }
+              }
+            },
           ),
         ),
       ),
